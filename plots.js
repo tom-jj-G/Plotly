6254d3d5dc4c37d69293187f18b5842af7ff11ad
Tom Greff <tomjj.greff@gmail.com>
2020-08-07 14:43:40 -0400

    upload files

--- plots.js
+++ plots.js
@@ -8,6 +8,7 @@ function init() {
     var sampleNames = data.names;
     var firstSubject = sampleNames[0];
     buildMetadata(firstSubject);
+    buildCharts(firstSubject);
     sampleNames.forEach((sample) => {
       selector
         .append("option")
@@ -25,7 +26,7 @@ init();
 // Updating the data when another subject is selected
 function optionChanged(newSample) {
   buildMetadata(newSample);
-  // buildCharts(newSample);
+  buildCharts(newSample);
 }
 
 
@@ -49,3 +50,42 @@ function buildMetadata(sample) {
     });
   });
 }
+
+
+// Building the charts
+function buildCharts(sample) {
+  console.log("buildCharts");
+  d3.json("samples.json").then((data) => {
+    console.log(`sample: ${sample}`);
+    // Filter the samples data to match the targetted sample
+    var sampleData = data.samples.filter(samples => samples.id == sample);
+    // Retrieve the top 10 otu_ids 
+    var top10Otu_idsArray = sampleData.map(data => data.otu_ids.slice(0, 10))[0];
+    // Retrieve the top 10 sample_values
+    var top10OSample_valuesArray = sampleData.map(data => data.sample_values.slice(0,10))[0];
+    // Retrieve the top 10 otu_labels
+    var top10Otu_labelsArray = sampleData.map(data => data.otu_labels.slice(0,10))[0];
+    //Create bar chart
+    createBarChart(top10Otu_idsArray,top10OSample_valuesArray,top10Otu_labelsArray);
+  });
+}
+
+function createBarChart(data1 ,data2, data3) {
+  // Formatting the otu_ids data
+  let top10Otu_idsArrayCoded = []
+  data1.reverse().forEach(data => {
+    let otu = 'OTU ' + data;
+    top10Otu_idsArrayCoded.push(otu);
+  });
+  // Building the plot
+  var trace = {
+    y: top10Otu_idsArrayCoded,
+    x: data2.reverse(),
+    text: data3.reverse(),
+    type: "bar",
+    orientation: 'h'
+  };
+  Plotly.newPlot("bar", [trace]);
+}
+
+
