30440aa0b40efdcd6c06f6ec4e63852d6ddc2030
Tom Greff <tomjj.greff@gmail.com>
2020-08-06 21:36:16 -0400

    upload files

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Plotly
+
+## Objectives
+
+The goals are to:
+
+- Create a bar chart of the top ten bacterial species in a volunteer’s navel. Use JavaScript to select only the most populous species.
+- Create a bubble chart to visualize the relative frequency of all the bacterial species found in a volunteer’s navel.
+- Complete the demographic information panel, if you have not done so.
--- plots.js
+++ plots.js
@@ -1,35 +1,51 @@
+// Building the subject ID number list
+// And displaying demographic info for the first subject
 function init() {
-    var selector = d3.select("#selDataset");
+  var selector = d3.select("#selDataset");
 
-    d3.json("samples.json").then((data) => {
-      console.log(data);
-      var sampleNames = data.names;
-      sampleNames.forEach((sample) => {
-        selector
-          .append("option")
-          .text(sample)
-          .property("value", sample);
-      });
-  })}
-
-
-init();
-
-
-function optionChanged(newSample) {
-  buildMetadata(newSample);
-  buildCharts(newSample);
+  d3.json("samples.json").then((data) => {
+    console.log(data);
+    var sampleNames = data.names;
+    var firstSubject = sampleNames[0];
+    buildMetadata(firstSubject);
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+  })
 }
 
 
+// Initiating the data
+init();
+
+
+// Updating the data when another subject is selected
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  // buildCharts(newSample);
+}
+
+
+// Building the demographic info
 function buildMetadata(sample) {
   d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    var result = resultArray[0];
+    var result = Object.entries(resultArray[0]);
+    var resultList = []
+    result.forEach(([key, value]) => {
+      let resultValue = key + ": " + value;
+      resultList.push(resultValue);
+    });
+    
     var PANEL = d3.select("#sample-metadata");
 
     PANEL.html("");
-    PANEL.append("h6").text(result.location);
+    resultList.forEach((result) => {
+      PANEL.append("h6").text(result);
+    });
   });
 }
